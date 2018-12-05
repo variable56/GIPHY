e170437efbd4bc7261f947ab623e239fd9190a51
variable56 <variable56@gmail.com>
2018-12-04 18:30:02 -0700

    button layout working

--- assets/javascript.js
+++ assets/javascript.js
@@ -8,10 +8,22 @@ let topics = ["Batman", "Superman", "Wonder Woman", "Joker", "Red Hood", "Scarec
 $(document).ready(function(){
 
 //For loop that will make a button for every term in the array
+function makeButtons() {
 for (i=0; i < topics.length; i++) {
     $("#buttonContainer").append("<button class=\"btn btn-primary btn-lg btn-block\">" + topics[i] + "</button>");
     }
+}
+makeButtons();
+$("#submit").on("click", function() {
 
+    // alert("working");
+    topics.push($("#searchTerm").val());
+    $("#buttonContainer").empty();
+    makeButtons();
+    
+
+
+});
 
     
 
