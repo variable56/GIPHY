3c38223aa4c20b8d7befe9e16bc78cd07748e99d
variable56 <variable56@gmail.com>
2018-12-05 15:52:27 -0700

    working api

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,6 +1,35 @@
 //Array for topics that will create buttons
 let topics = ["Batman", "Superman", "Wonder Woman", "Joker", "Red Hood", "Scarecrow"]
 
+function apiCall() {
+    
+    var host = "https://api.giphy.com/"
+    var path = "v1/gifs/search?&"
+    var q = "q=batman&"
+    var limit = "limit=10&"   
+    var key = "api_key=bSAuwMkKih45mkCg8HU18TqeCW3IRRmU";
+    
+    $.ajax({
+        url: host + path + q + limit + key,
+        method: "GET"
+    })
+
+    .then(function(response){
+        $("gifContainer").empty();
+        console.log(response);
+
+        for (i = 0; i < response.data.length; i++) {
+        
+        let newGif = $("<img>"); 
+        let stillImage = response.data[i].images.fixed_height_still.url;
+
+        newGif.attr("src", stillImage);
+
+        $("#gifContainer").append(newGif);
+        }
+
+    });      
+}
 
 
 
@@ -13,18 +42,21 @@ for (i=0; i < topics.length; i++) {
     $("#buttonContainer").append("<button class=\"btn btn-primary btn-lg btn-block\">" + topics[i] + "</button>");
     }
 }
+
 makeButtons();
+//make new buttons after searching
 $("#submit").on("click", function() {
 
-    // alert("working");
     topics.push($("#searchTerm").val());
     $("#buttonContainer").empty();
     makeButtons();
-    
-
-
 });
 
+});
+//click event for buttons to run the ajax call
+$(this).on("click", function(){
+    
+    apiCall();
+});
     
 
-})
