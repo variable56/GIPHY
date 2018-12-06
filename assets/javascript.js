bc697c5099319350e43ba5705e9ee6950d627f76
variable56 <variable56@gmail.com>
2018-12-05 23:28:20 -0700

    working buttons mostly

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,38 +1,6 @@
 //Array for topics that will create buttons
 let topics = ["Batman", "Superman", "Wonder Woman", "Joker", "Red Hood", "Scarecrow"]
 
-function apiCall() {
-    $("#gifContainer").empty();
-    var host = "https://api.giphy.com/"
-    var path = "v1/gifs/search?&"
-    var q = "q=" + "batman" + "&"
-    var limit = "limit=10&"   
-    var key = "api_key=bSAuwMkKih45mkCg8HU18TqeCW3IRRmU";
-    
-    $.ajax({
-        url: host + path + q + limit + key,
-        method: "GET"
-    })
-
-    .then(function(response){
-        $("gifContainer").empty();
-        console.log(response);
-
-        for (i = 0; i < response.data.length; i++) {
-        
-        let newGif = $("<img>"); 
-        let stillImage = response.data[i].images.fixed_height_still.url;
-        let rating = response.data[i].rating;
-        newGif.attr("src", stillImage);
-
-        // $("#gifContainer").append(newGif);
-        // $("#gifContainer").append(rating);
-
-        $("#gifContainer").append("<div class=\"card\" style=\"width: 18rem;\"><img class=\"card-img-top\" src=" + stillImage + " alt=\"Card image cap\"><div class=\"card-body\"><p class=\"card-text\">" + "Rated: " + rating + "</p></div></div>");
-        }
-
-    });      
-}
 
 
 
@@ -42,7 +10,7 @@ $(document).ready(function(){
 //For loop that will make a button for every term in the array
 function makeButtons() {
 for (i=0; i < topics.length; i++) {
-    $("#buttonContainer").append("<button class=\"btn btn-primary btn-lg btn-block\">" + topics[i] + "</button>");
+    $("#buttonContainer").append("<button data=\"" + topics[i] +  "\"class=\"btn btn-primary btn-lg btn-block\">" + topics[i] + "</button>");
     }
 }
 
@@ -55,11 +23,55 @@ $("#submit").on("click", function() {
     makeButtons();
 });
 
-});
+
 //click event for buttons to run the ajax call
-$(this).on("click", function(){
+$(".btn").on("click", function(){
     
-    apiCall();
+    $("#gifContainer").empty();
+    var host = "https://api.giphy.com/"
+    var path = "v1/gifs/search?&"
+    var q = "q=" + $(this).attr("data") + "&"
+    var limit = "limit=10&"   
+    var key = "api_key=bSAuwMkKih45mkCg8HU18TqeCW3IRRmU";
+    
+    $.ajax({
+        url: host + path + q + limit + key,
+        method: "GET"
+    })
+
+    .then(function(response){
+        console.log(response);
+        $("gifContainer").empty();
+        // console.log(topics[i]);
+
+        for (i = 0; i < response.data.length; i++) {
+        
+        let stillImage = response.data[i].images.fixed_height_still.url;
+        let rating = response.data[i].rating;
+        let newGif = $("#gifContainer").append("<div class=\"card\"  style=\"width: 18rem;\"><img  state=\"still\" data=\"" + topics[i] +  "\"class=\"card-img-top\" src=" + stillImage + " alt=\"Card image cap\"><div class=\"card-body\"><p class=\"card-text\">" + "Rated: " + rating + "</p></div></div>");
+        let animateImage = response.data[i].images.fixed_height.url
+
+        newGif.attr("src", stillImage);
+    }
+
+    });  
+
 });
-    
+//plays and pauses gif
+$("img").on("click", function() {
+    console.log(this);
+    if ($(this).attr("state") == "still") {
+        $(this).attr("src", ($(this).attr("src", animateImage)));
+        $(this).attr("state", "animate");
+      }
+      
+
+      if ($(this).attr("state") == "animate") {
+        $(this).attr("src", ($(this).attr("src", stillImage)));
+        $(this).attr("state", "animate");
+      }
+    });
+
+
+});
 
