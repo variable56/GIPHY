15fc0a35dbb5a86ab0bd7e84ac1a77494a78d1da
variable56 <variable56@gmail.com>
2018-12-07 21:38:39 -0700

    working pause and unpause

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,6 @@
 //Array for topics that will create buttons
 let topics = ["Batman", "Superman", "Wonder Woman", "Joker", "Red Hood", "Scarecrow"]
+let gifArray = [];
 
 
 
@@ -15,8 +16,9 @@ function makeButtons() {
 }
 
 makeButtons();
+
 //make new buttons after searching
-$("#submit").on("click", function () {
+$("#sidebar").on("click", "#submit", function () {
 
     topics.push($("#searchTerm").val());
     $("#buttonContainer").empty();
@@ -28,6 +30,7 @@ $("#submit").on("click", function () {
 //click event for buttons to run the ajax call
 $(".apiButton").on("click", function () {
 
+
     $("#gifContainer").empty();
     var host = "https://api.giphy.com/"
     var path = "v1/gifs/search?&"
@@ -38,46 +41,65 @@ $(".apiButton").on("click", function () {
     $.ajax({
         url: host + path + q + limit + key,
         method: "GET"
-    })
-
-        .then(function (response) {
-
-
-            //logs to the console the JSON object
-            console.log(response);
-            //empties the gifContainer of any previous images
-            $("gifContainer").empty();
-            //creates variables for the information in the object and then adds a card for all 10 items in the object along with all the attributes
-            for (i = 0; i < response.data.length; i++) {
-
-                let stillImage = response.data[i].images.fixed_height_still.url;
-                let animateImage = response.data[i].images.fixed_height.url
-                let rating = response.data[i].rating;
-                let title = response.data[i].title;
-                let giphyId = response.data[i].id;
-                newGif = $("#gifContainer").append("<div id=\"" + title + "\" class=\"card m-3\"  style=\"width: 18rem;\"><img id=\"" + giphyId + "\" state=\"still\" class=\"card-img-top\" src=" + stillImage + " still=\"" + stillImage + "\" animate=\"" + animateImage + "\" alt=\"Card image cap\"><div class=\"card-body\"><p class=\"card-text text-center border-warning\">" + "Rated: " + rating + "</p></div></div>");
-
-
-                //This is the click event that should start and stop the animation
-                $(title).on("click", function () {
-                    console.log(title);
-                    //$(this).attr("src", animateImage);
-
-                })
-
-
-            }
-
-        });
+    }).then(function (response) {
+            gifArray = [];
+            for (let i = 0; i <= response.data.length -1; i++) {
+            
+            gifArray.push({
+                stillImage: response.data[i].images.fixed_height_still.url,
+                animateImage: response.data[i].images.fixed_height.url,
+                rating: response.data[i].rating,
+                title: response.data[i].title,
+                giphyId: response.data[i].id
+            });
+            
+            // console.log(gifArray);
+        }
+        renderGif();
+    });
+    
 });
 
 
 
-// });
+function renderGif(response) {
+
+    
+    //logs to the console the JSON object
+    // console.log(response);
+    //empties the gifContainer of any previous images
+    $("gifContainer").empty();
+    //creates variables for the information in the object and then adds a card for all 10 items in the object along with all the attributes
+    for (i = 0; i < gifArray.length; i++) {
 
 
-// });
+    newGif = $("#gifContainer").append("<div id=\"" + gifArray[i].title + "\" state=\"still\" class=\"card m-3\"  style=\"width: 18rem;\"><img id=\"" + gifArray[i].giphyId + "\" state=\"still\" class=\"card-img-top\" src=" + gifArray[i].stillImage + " still=\"" + gifArray[i].stillImage + "\" animate=\"" + gifArray[i].animateImage + "\" alt=\"Card image cap\"><div class=\"card-body\"><p class=\"card-text text-center border-warning\">" + "Rated: " + gifArray[i].rating + "</p></div></div>");
+    
+    }
+        
+}
+
+$("#gifContainer").on("click", "img", function() {
+  
+if ($(this).attr("src") == $(this).attr("still")) {
+    $(this).attr("src", $(this).attr("animate"));
+    console.log($(this).attr("src"))
+    return;
+}
+if ($(this).attr("src") == $(this).attr("animate")) {
+    $(this).attr("src", $(this).attr("still"));
+    console.log($(this).attr("src"));
+    return;
+    }
+
+
+
+});
 
 //Can't get gifs to animate on clicking it has something to do with the fact that it's a bootstrap card im clicking on that has a nested img
 //When adding a new button, it breaks the ajax call for all buttons
 //the data attributes for each of my cards is going through the array
+
+//objects from ajax are in an array
+//need to make the buttons render from newly created array
+//onclick to animate images should be based on new array
